332249097a45fa3d2dc265232d5bf49a3479b49f
cristiAndreiTarasi <tarasicristia@gmail.com>
2019-02-23 23:48:22 +0000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,11 @@
 
 import React, { Component } from 'react';
-// Important - do not remove ***************
-import { hot } from 'react-hot-loader/root';
-// *****************************************
 
-// ***************************************** START
 const App = () => (
     <div className="jumbotron">
         <h1 className="display-4">React boilerplate <br />with Hot Loader plugin</h1>
         <p className="lead">Set up to work with sass and bootstrap.</p>
     </div>
 );
-// ***************************************** END
-
-// export as an argument to a hoc 
-export default hot(App);
+ 
+export default App;
